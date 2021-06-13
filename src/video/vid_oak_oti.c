09e5c8ae35720183e521d38520613ff9976084fd
EngiNerd89 <webmaster.crrc@yahoo.it>
2021-06-13 21:06:48 +0200

    Fixed Olivetti M300 onboard graphics

--- src/video/vid_oak_oti.c
+++ src/video/vid_oak_oti.c
@@ -31,8 +31,8 @@
 
 #define BIOS_037C_PATH			"roms/video/oti/bios.bin"
 #define BIOS_067_AMA932J_PATH	"roms/machines/ama932j/oti067.bin"
-#define BIOS_067_M300_08_PATH	"roms/machines/olivetti_m300_08/EVC_BIOS.ROM"
-#define BIOS_067_M300_15_PATH	"roms/machines/olivetti_m300_15/EVC_BIOS.ROM"
+#define BIOS_067_M300_08_PATH	"roms/machines/m30008/EVC_BIOS.ROM"
+#define BIOS_067_M300_15_PATH	"roms/machines/m30015/EVC_BIOS.ROM"
 #define BIOS_077_PATH			"roms/video/oti/oti077.vbi"
 
 
